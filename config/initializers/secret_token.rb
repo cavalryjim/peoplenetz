2a2643b9c729919c14042338a5b04d3bfcbd8b4b
James Davis <james.davisphd@gmail.com>
2012-04-22 20:34:49 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Peoplenetz::Application.config.secret_token = '543c8c55ec5f5078f20d0d5d069bd5478e2f37ce476f7510a691ef4911a0c2ae3e5200bf768a19f6d98c6b94e7fa7c2c4e75e41866795b5777cc165a505837da'
