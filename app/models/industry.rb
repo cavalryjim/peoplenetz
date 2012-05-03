62fb411b91da68881f22e4c8a6b79555d92d0f56
James Davis <james.davisphd@gmail.com>
2012-05-03 17:16:27 -0500

    Moved the active admin css file to vendor.

--- app/models/industry.rb
+++ app/models/industry.rb
@@ -1,5 +1,5 @@
 class Industry < ActiveRecord::Base
-  attr_accessible :description, :name, :pnetz_solution
+  attr_accessible :description, :name, :pnetz_response
   
   validates :name,  :presence       => true,
                     :uniqueness     => { :case_sensitive => false }
