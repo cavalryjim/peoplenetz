219fbecfd06a29e33d104101264b9dfb240e1bfc
James Davis <james.davisphd@gmail.com>
2012-05-16 17:46:44 -0500

    Made a ton of UI changes.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,8 @@
 module ApplicationHelper
   
+  def display_banner(image = 'other')
+    image = 'banners/' + image + '.jpg'
+    html = '<div class="banner-container"> <div class="container">' + image_tag(image) + '</div> </div>'
+  end
   
 end
