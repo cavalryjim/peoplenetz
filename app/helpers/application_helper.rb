e509ef420751aa6e98f38d82db3056cef9f05713
mjh113437 <mharrison@peoplenetz.com>
2012-06-05 04:50:24 +1000

    MJH

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   
   def display_banner(image = 'other', text = "Turn insight into business as usual")
-    image = 'banners/' + image + '.jpg'
+    image = 'banners/' + image + '.png'
     html = '<div class="banner-container"> <div class="container"> <div class="wrap">' 
     html = html + image_tag(image) 
     html = html + '<span class="comment">' + text + '</span>'
