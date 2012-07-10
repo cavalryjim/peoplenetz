fb1925f049fe5b6fe413eca863f97342d55877c8
James Davis <james.davisphd@gmail.com>
2012-07-10 13:29:29 -0500

    Changing some code to the old-style hash for ruby 1.8.7

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-Peoplenetz::Application.config.session_store :cookie_store, key: '_peoplenetz_session'
+# JDavis: ruby 1.8.7 does not support the new-style hash argument.  Changing for new hosting site.
+#Peoplenetz::Application.config.session_store :cookie_store, key: '_peoplenetz_session'
+Peoplenetz::Application.config.session_store :cookie_store, :key => '_peoplenetz_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
