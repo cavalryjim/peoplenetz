576b57f4246a398d0f5fee53066b902ead139522
mjh113437 <mharrison@peoplenetz.com>
2012-07-12 20:52:58 +1000

    MJH

--- app/mailers/notifications_mailer.rb
+++ app/mailers/notifications_mailer.rb
@@ -1,9 +1,8 @@
 class NotificationsMailer < ActionMailer::Base
-  default :from "website@peoplenetz.com"
-  
+
   def new_inquiry(contact)
     @contact = contact
 
-    mail(:to => "jdavis@peoplenetz.com, bharrison@peoplenetz.com, sales@peoplenetz.com", :subject => 'Peoplenetz: Information Request')
+    mail(:from "website@peoplenetz.com", :to => "jdavis@peoplenetz.com, bharrison@peoplenetz.com, sales@peoplenetz.com", :subject => 'Peoplenetz: Information Request')
   end
 end
